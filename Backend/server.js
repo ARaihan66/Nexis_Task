f1356b13d737ac0f305065b018b00bb93e93bdf3
Abu Raihan <aburaihancse066@gmail.com>
2022-12-05 10:41:43 +0600

    Server and data connection done

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = express();
+
+dotenv.config({
+    path: '/Backend/config/.env'
+});
+
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then((data) => {
+        console.log('Database Connected Successfully!!!')
+    }).catch((error) => {
+        console.log('Connection Failed!!!')
+    })
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server connection done")
+})
