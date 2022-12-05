a045715577c89455828f7295367688e6432090ca
Abu Raihan <aburaihancse066@gmail.com>
2022-12-05 11:55:59 +0600

    Add mothods in UserModel

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -3,17 +3,14 @@ const User = require('../Model/UserModel');
 //User Registration
 exports.createUser = async (req, res) => {
     const { firstname, lastname, email, phoneNo, password } = req.body;
-    let user = await User.findOne({ email: email });
-    if (user) {
+    let existingUser = await User.findOne({ email: email });
+    if (existingUser) {
         return res.status(400).json({
             success: false,
             message: "User Already Register With This email!!!"
         });
     }
-
-
-
-    const user = await User.create({
+    let user = await User.create({
         firstname: firstname,
         lastname: lastname,
         email: email,
@@ -26,3 +23,4 @@ exports.createUser = async (req, res) => {
         User: user
     })
 }
+
--- Backend/Model/UserModel.js
+++ Backend/Model/UserModel.js
@@ -1,4 +1,6 @@
 const { Schema, model } = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userSchema = Schema({
     firstname: {
@@ -23,5 +25,28 @@ const userSchema = Schema({
         required: true
     }
 }, { timestamps: true });
+
+//hash password
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+    let salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, parseInt(salt));
+});
+
+
+
+// jwt token
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({ id: this._id, email: this.email }, process.env.JWT_SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRES
+    });
+};
+
+//compare password
+userSchema.methods.comparePassword = async function (givenPassword) {
+    return await bcrypt.compare(givenPassword, this.password);
+}
 
 module.exports = model("User", userSchema);
